dc236a4225bc4a984ed4e9f1c776eada1cdcd1d7
fj <menspocky322@gmail.com>
2022-05-20 01:54:50 +0900

    ビルドエラーを解消

--- pages/api/example.ts
+++ pages/api/example.ts
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { downloadImages } from '../../lib/tools/download-images'
-import { getRandomEyecatch } from '../../lib/frontmatter'
+import { getRandomEyeCatch } from '../../lib/frontmatter'
 
 type Data = {
   name: string
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  getRandomEyecatch()
+  getRandomEyeCatch()
   res.status(200).json({ name: 'John Doe' })
 }
